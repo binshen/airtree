6434a87623071e796ef9c8b608289783f712d3bd
bin.shen <bin.shen@openx.com>
2016-08-05 16:20:25 +0800

    123123

--- app/src/main/java/com/moral/airtree/SplashActivity.java
+++ app/src/main/java/com/moral/airtree/SplashActivity.java
@@ -3,6 +3,8 @@ package com.moral.airtree;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
 
 public class SplashActivity extends Activity implements Runnable {
 
@@ -10,6 +12,11 @@ public class SplashActivity extends Activity implements Runnable {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+//        WindowManager windowManager = getWindowManager();
+//        Display display = windowManager.getDefaultDisplay();
+//        int screenWidth = screenWidth = display.getWidth();
+//        int screenHeight = screenHeight = display.getHeight();
 
         Thread delayThread = new Thread(this);
         delayThread.start();
